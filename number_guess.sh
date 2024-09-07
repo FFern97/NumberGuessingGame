3da490cdb5d032950ed0b72058357303ee3a2eaa
Facundo Fernández <ffernanndez97@gmail.com>
2024-09-07 19:29:12 -0300

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
 
 
 # Funcion para numero random: echo $(($RANDOM%100)) 
