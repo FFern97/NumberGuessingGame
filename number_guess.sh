cd7808458df95295ccc15812c6747b83be64946c
Facundo Fernández <ffernanndez97@gmail.com>
2024-09-08 12:36:35 -0300

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=postgres -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=users -t --no-align -c"
 
 echo "Enter your username:"
 read USERNAME
