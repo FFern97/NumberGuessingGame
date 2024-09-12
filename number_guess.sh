9bb8b84d13c1eeaeb213e28d286f6ab948c5cb89
Facundo Fernández <ffernanndez97@gmail.com>
2024-09-12 15:49:56 -0300

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -7,6 +7,7 @@ RANDOM_NUMBER(){
     echo $(($RANDOM%10+1))
 }
 
+
 #SAVE GENERATED NUMBER IN VARIABLE
 SECRET_NUMBER=$(RANDOM_NUMBER)
 
@@ -22,11 +23,11 @@ USER_EXIST=$($PSQL "SELECT name FROM users WHERE name = '$USERNAME' ");
 if [[ -z $USER_EXIST ]]
 then
     echo "Welcome, $USERNAME! It looks like this is your first time here." 
-    INSERTED=$($PSQL "INSERT INTO users (name) VALUES ('$USERNAME')");
+    INSERTED=$($PSQL "INSERT INTO users (name, games_played, best_game) VALUES ('$USERNAME', 0, NULL)");
   
 else
-    GAMES_PLAYED=$($PSQL "SELECT COUNT(games_played) FROM users")
-    BEST_GAME=$($PSQL "SELECT (best_game) FROM users")
+    GAMES_PLAYED=$($PSQL "SELECT games_played FROM users")
+    BEST_GAME=$($PSQL "SELECT best_game FROM users")
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
 fi
@@ -35,8 +36,6 @@ fi
 #GUESS NUMBER
 echo "Guess the secret number between 1 and 1000:"
 read USER_GUESS
-
-
 
 
 while [[ $USER_GUESS != $SECRET_NUMBER ]]
@@ -57,14 +56,17 @@ else
   then
     echo "It's higher than that, guess again:"
     read USER_GUESS
-
   fi
 
 fi  
 
 done
 
-echo "You guessed it in <number_of_guesses> tries. The secret number was $SECRET_NUMBER. Nice job!"
+UPDATE_GAMES=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE name = '$USERNAME' ")
+COUNT_GAMES=$(PSQL "SELECT games_played FROM users WHERE name = '$USERNAME'")
+echo "You guessed it in $COUNT_GAMES tries. The secret number was $SECRET_NUMBER. Nice job!"
+ 
+
 
 
 
