cfc21a73d5a672019895cee07afb541e7f2ad8d9
Facundo Fernández <ffernanndez97@gmail.com>
2024-09-12 13:02:14 -0300

    Update ps number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -2,37 +2,46 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-
 #GENERATE RANDOM NUMBER
 RANDOM_NUMBER(){
-    echo $(($RANDOM%1000))
-
+    echo $(($RANDOM%3))
 }
 
 #SAVE GENERATED NUMBER IN VARIABLE
-THIS_GAME_NUMBER=$(RANDOM_NUMBER)
-
+SECRET_NUMBER=$(RANDOM_NUMBER)
 
 #REQUEST AND READ USERNAME
 echo "Enter your username:"
 read USERNAME
 
-#SAVE USERNAME IN TABLE users:
-INSERTED=$($PSQL "INSERT INTO users (name) VALUES ('$USERNAME')");
-
-GAMES_PLAYED=$($PSQL "SELECT COUNT(games_played) FROM users")
-BEST_GAME=$($PSQL "SELECT COUNT(MIN(best_game)) FROM users")
-
+#CHECK IF USER EXISTS 
 USER_EXIST=$($PSQL "SELECT name FROM users WHERE name = '$USERNAME' ");
 
-if [[ $USER_EXIST != $USERNAME ]]
+
+#FIND OUT IF USERNAME EXISTs
+if [[ -z $USER_EXIST ]]
 then
-    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-    exit 0
+    echo "Welcome, $USERNAME! It looks like this is your first time here." 
+    INSERTED=$($PSQL "INSERT INTO users (name) VALUES ('$USERNAME')");
   
 else
-    echo "Welcome, $USERNAME! It looks like this is your first time here."
+    GAMES_PLAYED=$($PSQL "SELECT COUNT(games_played) FROM users")
+    BEST_GAME=$($PSQL "SELECT (best_game) FROM users")
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 
-fi  
+fi
 
 
+#GUESS NUMBER
+echo "Guess the secret number between 1 and 1000:"
+read USER_GUESS
+
+if [[ $USER_GUESS > $SECRET_NUMBER ]]
+then   
+  echo "It's lower than that, guess again:"
+  read USER_GUESS
+
+else
+  echo "Try again" 
+  read USER_GUESS
+fi
