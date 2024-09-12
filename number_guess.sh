423963089b75ede583128ec2a84447d738c9a883
Facundo Fernández <ffernanndez97@gmail.com>
2024-09-11 23:54:58 -0300

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -2,10 +2,23 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+
 echo "Enter your username:"
 read USERNAME
 
 INSERTED=$($PSQL "INSERT INTO users (name) VALUES ('$USERNAME')");
+
+USER_EXIST=$($PSQL "SELECT name FROM users WHERE name = '$USERNAME' ");
+
+if [[ $USER_EXIST != $USERNAME ]]
+then
+    echo "Welcome back, $USERNAME! You have played <games_played> games, and your best game took <best_game> guesses."
+    exit 0
+  
+else
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+
+fi  
 
 # Funcion para numero random: echo $(($RANDOM%100)) 
 
